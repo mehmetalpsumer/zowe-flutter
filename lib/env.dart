1022619fb0bff5c3dee5e2a599c9ccb1d2a91a8e
Mehmet Alp Sümer <mehmetalpsumer@gmail.com>
2019-12-10 14:04:38 +0300

    Add API endpoints

---
+++ lib/env.dart
@@ -0,0 +1,11 @@
+// Development API endpoints
+class DevEnv {
+  static const apiUrl = 'https://192.86.32.67:8547';
+  static const authUrl = 'https://192.86.32.67:8544/auth';
+}
+
+// Production API endpoints
+class ProdEnv {
+  static const apiUrl = 'https://192.86.32.67:8547';
+  static const authUrl = 'https://192.86.32.67:8544/auth';
+}
