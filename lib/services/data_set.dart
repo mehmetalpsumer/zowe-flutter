b78aaf00540c52719a469e57408387e26e102ff9
Mehmet Alp Sümer <mehmetalpsumer@gmail.com>
2019-12-11 16:42:42 +0300

    Update data set models

--- lib/services/data_set.dart
+++ lib/services/data_set.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:zowe_flutter/models/data_set.dart';
 import 'package:zowe_flutter/models/data_set_content.dart';
+import 'package:zowe_flutter/models/data_set_create.dart';
 import 'package:zowe_flutter/services/services.dart';
 
 class DataSetService {
@@ -47,5 +48,46 @@ class DataSetService {
         await ApiService.ioClient.put(url, headers: headers, body: dataJson);
 
     return response.statusCode == 200 || response.statusCode == 201;
+  }
+
+  /// Create a dataset
+  Future<bool> createDataSet(DataSetCreate dataSet) async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + AuthService.user.token,
+    };
+
+    final url = '${ApiService.DATA_SET_ENDPOINT}';
+    var response =
+        await ApiService.ioClient.post(url, headers: headers, body: dataSet.toJson());
+
+    return response.statusCode == 200 || response.statusCode == 201;
+  }
+
+  /// Delete data set
+  Future<bool> deleteDataSet(String dataSetName) async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + AuthService.user.token,
+    };
+
+    final url = '${ApiService.DATA_SET_ENDPOINT}/$dataSetName';
+    var response =
+        await ApiService.ioClient.delete(url, headers: headers);
+
+    return response.statusCode == 200 || response.statusCode == 204;
+  }
+
+  /// Get pds members
+  Future<List<String>> getDataSetMembers(String dataSetName) async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + AuthService.user.token,
+    };
+
+    final url = '${ApiService.DATA_SET_ENDPOINT}/$dataSetName/members';
+    var response =
+        await ApiService.ioClient.get(url, headers: headers);
+    return json.decode(response.body)['items'];
   }
 }
